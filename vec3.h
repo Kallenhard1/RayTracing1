da7400143e01093d91194bfe796d55c316cb37ad
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2021-05-26 13:11:36 -0300

    Create vec3.h

---
+++ vec3.h
@@ -0,0 +1,17 @@
+#ifndef VEC3_H	
+#define VEC3_H
+
+#include <c.math>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3() {
+	
+};
+
+// Type aliases for vec3
+using point3 = vec3;  // 3D point
+using color = vec3;   // rgb color
+
+#endif
